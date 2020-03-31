651f89c42bf1e39551026f0f77625b065c23eecd
abhisheksharma <62939441+AbhishekSharma16@users.noreply.github.com>
2020-04-01 01:23:13 +0530

    Update 1222.cpp

--- 1222.cpp
+++ 1222.cpp
@@ -9,15 +9,19 @@ class ARRAY
 
     ARRAY() : OverflowCounter(1), ElementCounter(0)
     {
-        AR= create(1);
+        std ::cout<<"enter constructor\n";
+        AR = create(1);
+        std ::cout<<"exit const\n";
+
     }
 
 
     int* create(int OverflowCounter)
     {
+
         int* ar= new int[OverflowCounter*10];
         return ar;
-        delete ar;
+
     }
 
 
@@ -28,23 +32,34 @@ class ARRAY
         for (int i=0 ; i<=ElementCounter ; i++)
             *(ar+i)=*(AR+i);
 
+
         *AR=*ar;
-        delete ar;
+       // delete ar;
 
     }
 
 
-    void Insert(int value)
+    void Insert()
     {
         if ( ElementCounter <= OverflowCounter * 10 )
         {
+            std::cout<<"enter element";
+            int value;
+            std::cin>>value;
+            //std::cout<<ElementCounter<<"\n";
             *(AR+ElementCounter) = value;
             ElementCounter++;
+            std::cout<<*(AR+ElementCounter)<<"\n"<<"inserted\n";
+
         }
-        else
-            overflow();
-            *(AR+ElementCounter) = value;
-            ElementCounter++;
+       /* else
+        {
+           overflow();
+           *(AR+ElementCounter) = value;
+           ElementCounter++;
+           std::cout<<*(AR+ElementCounter)<<"\n"<<"inserted overflow";
+        }*/
+
 
     }
 
@@ -54,16 +69,14 @@ class ARRAY
         delete AR ;
     }
 };
+
 int main()
 {
+
     std::cout<<"hi"<<std::endl;
 
 	ARRAY a;
-	a.Insert(1);
-
-	std::cout<<*(a.AR+1);//////////////////
-
-	a.Insert(10);
+	a.Insert();
 
     return 0;
 }
